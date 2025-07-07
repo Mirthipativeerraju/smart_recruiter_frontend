7f144a218d47b4c7cc0cdb1b30fe8d0078edfead
Mirthipativeerraju <veerrajumirthipati@gmail.com>
2025-07-07 18:19:23 +0530

    Update

--- src/components/user/UserLogin.js
+++ src/components/user/UserLogin.js
@@ -24,7 +24,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://smart-recruiter-backend.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
